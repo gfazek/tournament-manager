22d4c5a54b98423bbe6b3a036332e7ecb71e442f
Gergo Fazekas <gergo.fazekas@ni.com>
2017-10-10 09:22:15 +0200

    BaseEntity added

---
+++ tournamentapp-data/src/main/java/hu/unideb/gergofazekas/entity/BaseEntity.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.unideb.gergofazekas.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ *
+ * @author gfazekas
+ */
+
+@MappedSuperclass
+public class BaseEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+}
