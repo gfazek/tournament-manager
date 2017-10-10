39388ffbe7da93f03d1a7b1b993147722deffd3d
Gergo Fazekas <gergo.fazekas@ni.com>
2017-10-10 09:24:23 +0200

    gender and person vo added

---
+++ tournamentapp-service-api/src/main/java/hu/unideb/gergofazekas/vo/GenderVo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.unideb.gergofazekas.vo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author gfazekas
+ */
+public enum GenderVo implements Serializable{
+    MALE, FEMALE
+}
