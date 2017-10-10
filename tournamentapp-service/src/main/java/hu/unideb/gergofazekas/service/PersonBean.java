3e78f8c48f0498e3d6c1aef838f45356f7d9831c
Gergo Fazekas <gergo.fazekas@ni.com>
2017-10-11 00:03:32 +0200

    createperson method implemented

--- tournamentapp-service/src/main/java/hu/unideb/gergofazekas/service/PersonBean.java
+++ tournamentapp-service/src/main/java/hu/unideb/gergofazekas/service/PersonBean.java
@@ -6,6 +6,8 @@
 package hu.unideb.gergofazekas.service;
 
 import hu.unideb.gergofazekas.entity.PersonEntity;
+import hu.unideb.gergofazekas.utility.Gender;
+import hu.unideb.gergofazekas.vo.GenderVo;
 import hu.unideb.gergofazekas.vo.PersonVo;
 import javax.ejb.Stateless;
 import javax.inject.Named;
@@ -25,6 +27,15 @@ public class PersonBean implements PersonServiceLocal{
     
     @Override
     public PersonVo createPerson(PersonVo personVo) {
+        PersonEntity personEntity = new PersonEntity();
+        personEntity.setUsername(personVo.getUsername());
+        personEntity.setEmail(personVo.getEmail());
+        personEntity.setPassword(personVo.getPassword());
+        personEntity.setFirstName(personVo.getFirstName());
+        personEntity.setLastName(personVo.getLastName());
+        personEntity.setGender(Gender.valueOf(personVo.getGender().name()));
+        personEntity.setDob(personVo.getDob());
+        em.persist(personEntity);
         return personVo;
     }
     
